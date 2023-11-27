3e6c98c609ae5b6f0869d13386746ed3904e7d9b
ncioffi1 <imnickcioffi@gmail.com>
2023-11-26 19:55:47 -0500

    m

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.117.1/build/three.module.js';
+// import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
